1d88937e2b761be0f97cf4c5b963184e371a0a7e
Cheezy Butternut <cheezybutternutmunchkin@gmail.com>
2026-01-15 21:12:21 +0800

    - cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField, EmbedBuilder, AttachmentBuilder, GuildMember, ActivityType, Partials } = require('discord.js');
+const {Client, IntentsBitField, EmbedBuilder, AttachmentBuilder, GuildMember, ActivityType, Partials } = require('discord.js');
 
 const via = new Client ({
     intents: [
@@ -30,20 +30,20 @@ via.on('guildMemberAdd', (member) => {
 
     member.roles.add(process.env.LOCAL_ROLE_ID);
 
-if (welcome_channel) {
-    const welcomeEmbed = new EmbedBuilder()
-    .setColor('DarkButNotBlack')
-    .setThumbnail('attachment://dance_bois_welcome.gif')
-    .setDescription(`\u200b\nPlease check out these channels:\n<#${ruleschannelID}>\n<#${roleschannelID}>\n\u200b`)
-    .setImage('attachment://banner.jpg')
+    if (welcome_channel) {
+        const welcomeEmbed = new EmbedBuilder()
+        .setColor('DarkButNotBlack')
+        .setThumbnail('attachment://dance_bois_welcome.gif')
+        .setDescription(`\u200b\nPlease check out these channels:\n<#${ruleschannelID}>\n<#${roleschannelID}>\n\u200b`)
+        .setImage('attachment://banner.jpg')
 
-welcome_channel.send({
-    content: `**Welcome to the server** <@${member.id}>`,
-    embeds: [welcomeEmbed],
-    files: [lookbanner, dance_bois]
-});
+    welcome_channel.send({
+        content: `**Welcome to the server** <@${member.id}>`,
+        embeds: [welcomeEmbed],
+        files: [lookbanner, dance_bois]
+    });
     console.log(`${member.user.username} has joined the server master`)
-}
+    }
 });
 
 via.on('guildMemberRemove', (member) => {
@@ -58,65 +58,45 @@ via.on('guildMemberRemove', (member) => {
         .setTitle(`\ub200\n${member.user.username} has left the server`)
         .setDescription('Thanks for hanging out with us!\n\ub200 ')
 
-welcome_channel.send({
-    embeds: [goodbyeEmbed],
-    files: [goodbye]
-});
+    welcome_channel.send({
+        embeds: [goodbyeEmbed],
+        files: [goodbye]
+    });
     console.log(`${member.user.username} has left the server`)
     }
 });
 
-via.on('messageCreate', (message) => {
-    if(message.author.bot) {
-        return;
-    }
-    if (message.content === 'yes') {
-        message.reply('yes');
-    }
-});
+const roleList = {
+    // [emoji id] : role id
+    [process.env.MC_ROLE_EMOJI_ID] : process.env.MC_ROLE_ID,
+    [process.env.MC_ANNOUNCEMENTS_ROLE_EMOJI_ID] : process.env.MC_ANNOUNCEMENTS_ROLE_ID,
+    [process.env.MC_PATCH_NOTES_ROLE_EMOJI_ID] : process.env.MC_PATCH_NOTES_ROLE_ID,
+    [process.env.MC_IG_CHAT_ROLE_EMOJI_ID] : process.env.MC_IG_CHAT_ROLE_ID,
+    [process.env.MC_JAVA_EMOJI_ID] : process.env.MC_JAVA_ROLE_ID,
+    [process.env.MC_BEDROCK_EMOJI_ID] : process.env.MC_BEDROCK_ROLE_ID
+}
 
 via.on('messageReactionAdd', async (reaction, user) => {
+    const rolemessageID = process.env.ROLE_REACT_MESSAGE_ID;
+    const roleId = roleList[reaction.emoji.id];
+    const member = await reaction.message.guild.members.fetch(user.id);
 
-    const reactmessagelID = process.env.ROLE_REACT_MESSAGE_ID;
-    const mcRoleID = process.env.MC_ROLE_ID;                            // MC role
-    const mcAnnRoleID = process.env.MC_ANNOUNCEMENTS_ROLE_ID;           // MC announce
-    const mcPanRoleID = process.env.MC_PATCH_NOTES_ROLE_ID;             // MC patch notes
-    const mcChatRoleID = process.env.MC_IG_CHAT_ROLE_ID;                // Mc chat
-    const mcRoleEmojiID = process.env.MC_ROLE_EMOJI_ID;
-    const mcAnnEmojiID= process.env.MC_ANNOUNCEMENTS_ROLE_EMOJI_ID;
-    const mcPanEmojiID= process.env.MC_PATCH_NOTES_ROLE_EMOJI_ID;
-    const mcChatEmojiID= process.env.MC_IG_CHAT_ROLE_EMOJI_ID;
-    
     if (reaction.partial) {
         try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error('Something went horibly wrong') }
+            await reaction.fetch() } 
+        catch (error) {
+            console.log('Something went wrong master') }
+    }
+
+    if (reaction.message.id === rolemessageID ) {
+        if (roleId) {
+            try {
+                await member.roles.add(roleId)
+            } catch (error) {
+                console.log('failed to add role')
+            }
         }
-
-        const member = await reaction.message.guild.members.fetch(user.id);
-
-        if (reaction.message.id === reactmessagelID) {
-
-            if (reaction.emoji.id === mcRoleEmojiID) {
-                member.roles.add(mcRoleID);
-                console.log('role has been added')
-            }
-            if (reaction.emoji.id === mcAnnEmojiID) {
-                member.roles.add(mcAnnRoleID);
-                console.log('role has been added')
-            }
-            if (reaction.emoji.id === mcPanEmojiID) {
-                member.roles.add(mcPanRoleID)
-                console.log('role has been added')
-            }
-            if (reaction.emoji.id === mcChatEmojiID) {
-                member.roles.add(mcChatRoleID)
-                console.log('role has been added')
-            }
-
-        }
-
-    });
+    }
+});
 
 via.login(process.env.TOKEN);
