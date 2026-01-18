5b43debe50ce7801810fc31d54b93b64f9db2ad2
Cheezy Butternut <cheezybutternutmunchkin@gmail.com>
2026-01-18 21:26:57 +0800

    updated role reaction add
    currently having issues with remove react to remove role.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {Client, IntentsBitField, EmbedBuilder, AttachmentBuilder, GuildMember, ActivityType, Partials, } = require('discord.js');
+const {Client, IntentsBitField, EmbedBuilder, AttachmentBuilder, GuildMember, ActivityType, Partials} = require('discord.js');
 
 const via = new Client ({
     intents: [
@@ -8,11 +8,13 @@ const via = new Client ({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
         IntentsBitField.Flags.GuildMessageReactions
+    
     ],
     partials: [
         Partials.Channel,
         Partials.Message,
-        Partials.Reaction
+        Partials.Reaction,
+        Partials.User
     ]
 });
 
@@ -61,8 +63,8 @@ via.on('guildMemberRemove', (member) => {
         const goodbyeEmbed = new EmbedBuilder()
         .setColor('DarkGrey')
         .setThumbnail('attachment://perdo_goodbye.gif')
-        .setTitle(`\ub200\n${member.user.username} has left the server`)
-        .setDescription('Thanks for hanging out with us!\n\ub200 ')
+        .setTitle(`\u200b\n${member.user.username} has left the server`)
+        .setDescription('Thanks for hanging out with us!\n\u200b')
 
     welcome_channel.send({
         embeds: [goodbyeEmbed],
@@ -72,76 +74,139 @@ via.on('guildMemberRemove', (member) => {
     }
 });
 
-const roleList = {
-    // [emoji id] : role id
-    [process.env.MC_EMOJI] : process.env.MC_ROLE,
-    [process.env.MC_ANNOUNCEMENTS_EMOJI] : process.env.MC_ANNOUNCEMENTS_ROLE,
-    [process.env.MC_PATCH_NOTES_EMOJI] : process.env.MC_PATCH_NOTES_ROLE,
-    [process.env.MC_IG_CHAT_EMOJI] : process.env.MC_IG_CHAT_ROLE,
-    [process.env.MC_JAVA_EMOJI] : process.env.MC_JAVA_ROLE,
-    [process.env.MC_BEDROCK_EMOJI] : process.env.MC_BEDROCK_ROLE,
-    [process.env.ART_EMOJI] : process.env.ART_ROLE,
-    [process.env.ART_YAPS_EMOJI] : process.env.ART_YAPS_ROLE,
-    [process.env.ART_BIBLE_EMOJI] : process.env.ART_BIBLE_ROLE
+
+
+
+
+
+
+
+
+const mainRole = {
+    mc: process.env.MC_ROLE,
+    art: process.env.ART_ROLE
 }
 
+const rolesID = {
+    [process.env.MC_EMOJI] : {
+        roleID: mainRole.mc },
+
+    [process.env.MC_ANNOUNCEMENTS_EMOJI]: {
+        roleID: process.env.MC_ANNOUNCEMENTS_ROLE,
+        reqRole: mainRole.mc },
+
+    [process.env.MC_PATCH_NOTES_EMOJI]: {
+        roleID: process.env.MC_PATCH_NOTES_ROLE,
+        reqRole: mainRole.mc },
+
+    [process.env.MC_IG_CHAT_EMOJI]: {
+        roleID: process.env.MC_IG_CHAT_ROLE,
+        reqRole: mainRole.mc },
+
+    [process.env.MC_JAVA_EMOJI]: {
+        roleID: process.env.MC_JAVA_ROLE,
+        reqRole: mainRole.mc },
+
+    [process.env.MC_BEDROCK_EMOJI]: {
+        roleID: process.env.MC_BEDROCK_ROLE,
+        reqRole: mainRole.mc },
+
+    [process.env.ART_EMOJI] : {
+        roleID: mainRole.art },
+
+    [process.env.ART_YAPS_EMOJI]: {
+        roleID: process.env.ART_YAPS_ROLE,
+        reqRole: mainRole.art },
+
+    [process.env.ART_BIBLE_EMOJI]: {
+        roleID: process.env.ART_BIBLE_ROLE,
+        reqRole: mainRole.art
+    }
+}
+
+
 via.on('messageReactionAdd', async (reaction, user) => {
-    const rolemessageID = process.env.ROLE_REACT_MESSAGE_ID;
-    const roleId = roleList[reaction.emoji.id];
-    const member = await reaction.message.guild.members.fetch(user.id);
+    const reactMessageID = process.env.ROLE_REACT_MESSAGE_ID;
+    const check = rolesID[reaction.emoji.id];
+    const member = await reaction.message.guild.members.fetch(user.id)
 
     if (reaction.partial) {
         try {
-            await reaction.fetch() } 
-        catch (error) {
-            console.log('Something went wrong master') }
+            await reaction.fetch();
+        } catch (error) {
+            console.log('failed to fetch user reaction id')
+        }
     }
 
-    if (reaction.message.id === rolemessageID ) {
-        if (roleId) {
-            try {
-                await member.roles.add(roleId)
-                console.log('role has been added')
-            } catch (error) {
-                console.log('failed to add role')
-            }
+    if (reaction.message.id === reactMessageID) {
+
+        if (check.reqRole && !member.roles.cache.has(check.reqRole)) {
+            await reaction.users.remove(user);
+            console.log(`${member.user.username} does not have the required role`)
+            return;
         }
+        await member.roles.add(check.roleID)
+        console.log(`role has been added to ${member.user.username}`)
     }
 });
 
-const mainRole = {
+/*const mainRemove = {
+    [rolesID.MC_EMOJI] : [
+        rolesID.MC_ROLE,
+        rolesID.MC_ANNOUNCEMENTS_ROLE,
+        rolesID.MC_PATCH_NOTES_ROLE,
+        rolesID.MC_IG_CHAT_ROLE,
+        rolesID.MC_JAVA_ROLE,
+        rolesID.MC_BEDROCK_ROLE
+    ],
+    [rolesID.ART_EMOJI] : [
+        rolesID.ART_ROLE,
+        rolesID.ART_BIBLE_ROLE,
+        rolesID.ART_YAPS_ROLE
+    ]
+}
+*/
+
+const mainRemove = {
     //Minecraft Main Role
-    [process.env.MC_EMOJI] : [process.env.MC_ROLE, 
+    [process.env.MC_ROLE_EMOJI_ID] : [
+        process.env.MC_ROLE, 
         process.env.MC_ANNOUNCEMENTS_ROLE, 
         process.env.MC_PATCH_NOTES_ROLE,
         process.env.MC_IG_CHAT_ROLE,
         process.env.MC_BEDROCK_ROLE,
         process.env.MC_JAVA_ROLE],
     //Art Main Role
-    [process.env.ART_EMOJI] : [process.env.ART_ROLE,
+    [process.env.ART_EMOJI] : [
+        process.env.ART_ROLE,
         process.env.ART_YAPS_ROLE,
-        process.env.ART_BIBLE_ROLE]
+        process.env.ART_BIBLE_ROLE
+    ]
 }
 
 via.on('messageReactionRemove', async (reaction, user) => {
-    const rolemessageID = process.env.ROLE_REACT_MESSAGE_ID;
-    const mainID = mainRole[reaction.emoji.id];
-    const roleID = roleList[reaction.emoji.id];
+    const reactMessageID = process.env.ROLE_REACT_MESSAGE_ID;
+    const main = mainRemove[reaction.emoji.id];
+    const subroles = rolesID[reaction.emoji.id];
     const member = await reaction.message.guild.members.fetch(user.id);
+
+    if (user.bot) return;
 
     if (reaction.partial) {
         try {
             await reaction.fetch();
         } catch (error) {
-            console.log('RoleRemove - something went wrong master') }
+            console.log('failed to fetch user reaction id')
+        }
     }
-    if (reaction.message.id === rolemessageID) {
-        if (mainID) {
-            member.roles.remove(mainID)
+
+    if (reaction.message.id === reactMessageID) {
+         if (main) {
+            await member.roles.remove(main)
             console.log('role has been removed')
         }
-        if (roleID) {
-            member.roles.remove(roleID)
+        if (subroles) {
+            await member.roles.remove(subroles.roleID)
             console.log('role has been removed')
         }
     }
